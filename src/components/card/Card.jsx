90b9f565290dfcdee3cafe929c5c52845baeab4f
RizWaaN3024 <rizwaanans786@gmail.com>
2024-07-21 20:46:03 +0530

    Added ListPage Stylings

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -16,6 +16,27 @@ const Card = ({item}) => {
           <img src="/pin.png" alt="" />
           <span>{item.address}</span>
         </p>
+        <p className='price'>$ {item.price}</p>
+        <div className="bottom">
+          <div className="features">
+            <div className="feature">
+            <img src="./bed.png" alt="" />
+            <span>{item.bedroom} bedroom</span>
+            </div>
+            <div className="feature">
+              <img src="/bath.png" alt="" />
+              <span>{item.bathroom} bathroom</span>
+            </div>
+          </div>
+          <div className="icons">
+            <div className="icon">
+              <img src="./save.png" alt="" />
+            </div>
+            <div className="icon">
+              <img src="./chat.png" alt="" />
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   )
