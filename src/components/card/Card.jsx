4801cd1a42ca76d20240a31e8a12fffc870de824
RizWaaN3024 <rizwaanans786@gmail.com>
2024-07-21 19:11:58 +0530

    Added Cards

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import "./card.scss"
+import { Link } from 'react-router-dom'
 
-const Card = () => {
+const Card = ({item}) => {
   return (
-    <div>
-      Card
+    <div className='card'>
+      <Link to={`/${item.id}`} className='imageContainer'>
+        <img src={item.img} alt="" />
+      </Link>
+      <div className='textContainer'>
+        <h2 className='title'>
+          <Link to={`/${item.id}`}>{item.title}</Link>
+        </h2>
+        <p className='address'>
+          <img src="/pin.png" alt="" />
+          <span>{item.address}</span>
+        </p>
+      </div>
     </div>
   )
 }
