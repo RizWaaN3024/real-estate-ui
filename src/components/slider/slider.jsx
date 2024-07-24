39df13f13e98da2e44c554e1cfaa840f8aaa1e73
RizWaaN3024 <rizwaanans786@gmail.com>
2024-07-24 23:56:30 +0530

    Slider initial setup

---
+++ src/components/slider/slider.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./slider.scss"
+
+const Slider = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Slider
--- src/routes/singlePage/singlePage.jsx
+++ src/routes/singlePage/singlePage.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
 import "./singlePage.scss"
+import Slider from '../../components/slider/slider'
 
 const SinglePage = () => {
   return (
     <div className='singlePage'>
-      <div className='details'></div>
-      <div className='features'></div>
+      <div className='details'>
+        <div className="wrapper">
+          <Slider />
+          <div className="info">
+            <div className="top"></div>
+            <div className="bottom"></div>
+          </div>
+        </div>
+      </div>
+      <div className='features'>
+        <div className="wrapper"></div>
+      </div>
     </div>
   )
 }
