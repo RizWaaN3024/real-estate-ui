ccae0c9ba24c238ee0f228806ec00b2a3d945ec9
RizWaaN3024 <rizwaanans786@gmail.com>
2024-07-25 00:10:41 +0530

    Added styling for singlePage

--- src/routes/singlePage/singlePage.jsx
+++ src/routes/singlePage/singlePage.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import "./singlePage.scss"
 import Slider from '../../components/slider/slider'
+import { singlePostData, userData } from "../../lib/dummydata"
 
 const SinglePage = () => {
   return (
@@ -9,7 +10,22 @@ const SinglePage = () => {
         <div className="wrapper">
           <Slider />
           <div className="info">
-            <div className="top"></div>
+            <div className="top">
+              <div className="post">
+                <h1>{singlePostData.title}</h1>
+                <div className="address">
+                  <img src="/pin.png" alt="" />
+                  <span>{singlePostData.address}</span>
+                </div>
+                <div className="price">
+                  $ {singlePostData.price}
+                </div>
+              </div>
+              <div className="user">
+                <img src={userData.img} alt="" />
+                <span>{userData.name}</span>
+              </div>
+            </div>
             <div className="bottom"></div>
           </div>
         </div>
