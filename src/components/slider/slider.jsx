ccca2fe5a1760a6720f90a34b4f9e7fb9174466c
RizWaaN3024 <rizwaanans786@gmail.com>
2024-07-26 01:00:31 +0530

    Added Slider image

--- src/components/slider/slider.jsx
+++ src/components/slider/slider.jsx
@@ -4,6 +4,18 @@ import "./slider.scss"
 const Slider = ({images}) => {
   return (
     <div className='slider'>
+      <div className="fullSlider">
+        <div className="arrow">
+          <img src="/arrow.png" alt="" />
+        </div>
+        <div className="imgContainer">
+          <img src={images[0]} alt="" />
+        </div>
+        <div className="arrow">
+          <img src="/arrow.png" className='right' alt="" />
+        </div>
+        <div className='close'>X</div>
+      </div>
         <div className="bigImage">
             <img src={images[0]} alt="" />
         </div>
