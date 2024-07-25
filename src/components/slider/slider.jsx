9f33cc55edc8862ca8bb2913c400285bc8f854f2
RizWaaN3024 <rizwaanans786@gmail.com>
2024-07-26 01:11:20 +0530

    Added Slider functionality

--- src/components/slider/slider.jsx
+++ src/components/slider/slider.jsx
@@ -1,27 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./slider.scss"
 
 const Slider = ({images}) => {
+  const [imageIndex, setImageIndex] = useState(null);
   return (
     <div className='slider'>
+      {imageIndex !== null && (
       <div className="fullSlider">
         <div className="arrow">
           <img src="/arrow.png" alt="" />
         </div>
         <div className="imgContainer">
-          <img src={images[0]} alt="" />
+          <img src={images[imageIndex]} alt="" />
         </div>
         <div className="arrow">
           <img src="/arrow.png" className='right' alt="" />
         </div>
         <div className='close'>X</div>
       </div>
+      )}
         <div className="bigImage">
-            <img src={images[0]} alt="" />
+            <img src={images[0]} alt="" onClick={() => setImageIndex(0)} />
         </div>
         <div className="smallImages">
             {images.slice(1).map((image, index) => (
-                <img src={image} alt="" key={index} />
+                <img src={image} alt="" key={index} onClick={() => setImageIndex(index)} />
             ))}
         </div>
     </div>
