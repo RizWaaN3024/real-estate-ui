e6b22f5de00f6b723dbdefce370b45b78dcf384d
RizWaaN3024 <rizwaanans786@gmail.com>
2024-07-31 01:35:40 +0530

    ChatBox setup

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -32,7 +32,53 @@ const Chat = () => {
                 <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
             </div>
         </div>
-        <div className='chatBox'></div>
+        <div className='chatBox'>
+            <div className="top">
+                <div className="user">
+                    <img src="https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" />
+                    John Doe
+                </div>
+                <span className='close'>X</span>
+            </div>
+            <div className="center">
+                <div className="chatMessage">
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing.</p>
+                    <span>1 hour ago</span>
+                </div>
+                <div className="chatMessage own">
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing.</p>
+                    <span>1 hour ago</span>
+                </div>
+                <div className="chatMessage">
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing.</p>
+                    <span>1 hour ago</span>
+                </div>
+                <div className="chatMessage own">
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing.</p>
+                    <span>1 hour ago</span>
+                </div>
+                <div className="chatMessage">
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing.</p>
+                    <span>1 hour ago</span>
+                </div>
+                <div className="chatMessage own">
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing.</p>
+                    <span>1 hour ago</span>
+                </div>
+                <div className="chatMessage">
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing.</p>
+                    <span>1 hour ago</span>
+                </div>
+                <div className="chatMessage own">
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing.</p>
+                    <span>1 hour ago</span>
+                </div>
+            </div>
+            <div className="bottom">
+                <textarea name="" id=""></textarea>
+                <button>Send</button>
+            </div>
+        </div>
     </div>
   )
 }
